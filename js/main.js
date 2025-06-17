6b7e73714b8d621fe8c769ddbc7db73421ec922e
Gustavo Billoud <gustavobilloud@gmail.com>
2025-06-17 17:23:39 -0300

    practica variables y console

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let nombre = prompt("ingrese su nombre:");
+let apellido = prompt("ingrese su apellido");
+let edad = Number(prompt("ingrese su edad"));
+//console.log(nombre, apellido, edad)
+alert(apellido)
